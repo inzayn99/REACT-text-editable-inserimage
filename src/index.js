192cb6e5cf1361015c69e8fba29c3564b76a3ab9
Firoj <mymindhungry@gmail.com>
2021-09-13 16:10:53 +0545

    React-editable text & inserting image

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name = "zayn";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const links = "https//www.google.com";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+  <h1 contentEditable='true'> My name is {name} </h1>
+  
+  <img src={img1} alt="randomimage"/>
+  <img src={img2} alt="randomimage"/>
+  
+  <a href={links} target="_blank"><img src={img3} alt="randomimage"/></a>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+  
+  </>,
+  document.getElementById("root")
+);
